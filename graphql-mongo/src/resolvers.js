cce84dd83d022a85eb6f6779b3c5793ff15f5eae
dashfuentes <smis117112@gmail.com>
2022-11-28 21:10:37 -0600

    add update mutation code

--- graphql-mongo/src/resolvers.js
+++ graphql-mongo/src/resolvers.js
@@ -7,14 +7,21 @@ export const resolvers = {
         async getCourses() {
             const courses = await Course.find();
             return courses;
-        }
+        },
+        async getCourseById( _, { id } ) {
+             return await Course.findById(id)
+         }
     },
 
     Mutation: {
         async createCourse( _, { input } ) {
             //{title: test, description: destest.........}
-          return await  Course.create(input)
+            return await Course.create( input )
             
+        },
+
+        async updateCourse( _, { input, _id } ) {
+            return await Course.findByIdAndUpdate(_id, input, {new: true})
         }
     }
 
--- graphql-mongo/src/schema.js
+++ graphql-mongo/src/schema.js
@@ -6,10 +6,13 @@ import {resolvers} from "./resolvers"
 const TypeDefs = `
     type Query {
       getCourses : [Course]
+      getCourseById(id: ID) : Course
     }
 
     type Mutation {
         createCourse(input: CourseInput ) : Course
+        updateCourse(input: CourseInput, _id : ID ) : Course
+        removeCourse(): [Course]
     }
     type Course {
         _id : ID
@@ -29,6 +32,15 @@ const TypeDefs = `
 
 
 `
+
+var test = 1;
+var tes2 = 2
+
+function sum( num1, num2 ) {
+    return num1 + num2
+}
+
+sum(test,tes2)
 
 export default makeExecutableSchema( {
     typeDefs: TypeDefs,
